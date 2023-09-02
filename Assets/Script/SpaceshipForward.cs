51cdb233e11947c0d4e62f6512d47b6f4ce0bb16
Buse Yılmaz <bsylm@outlook.com>
2023-09-02 18:59:19 +0300

    Yapılan
    Pistol,ScenePass,SpacespinController

---
+++ Assets/Script/SpaceshipForward.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceshipForward : MonoBehaviour
+{
+    [SerializeField] private float hiz = 3.0f; // Geminin hýzýný ayarlayýn.
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        // Ýlerleme vektörünü hesaplayýn (örneðin, X ekseninde düz ilerleme).
+        Vector3 ilerlemeVektoru = Vector3.right * hiz;
+
+        // Gemiyi bu vektördeki hýzda sürekli olarak ilerletin.
+        rb.velocity = ilerlemeVektoru;
+    }
+}
