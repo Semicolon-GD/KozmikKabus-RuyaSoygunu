d361869d7b7d057e9309b9c9aa2d2d5f500db492
Baris34 <46277882+Baris34@users.noreply.github.com>
2023-09-02 19:23:51 +0300

    CameraControllerAdded

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -7,17 +7,23 @@ public class InputManager : MonoBehaviour
     private PlayerInput playerInput;
     private PlayerInput.OnFootActions onFoot;
     private PlayerMotor playerMotor;
+    private PlayerLook playerLook;
     void Awake()
     {
         playerInput=new PlayerInput();
         onFoot=playerInput.OnFoot;
         playerMotor=GetComponent<PlayerMotor>();
+        playerLook=GetComponent<PlayerLook>();
         onFoot.Jump.performed += ctx => playerMotor.Jump();
     }
 
     private void FixedUpdate()
     {
         playerMotor.ProcessMove(onFoot.Movement.ReadValue<Vector2>());
+    }
+    private void LateUpdate()
+    {
+        playerLook.ProcessLook(onFoot.Look.ReadValue<Vector2>());
     }
     void Update()
     {
