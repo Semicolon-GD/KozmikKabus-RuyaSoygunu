8aaed74734a947349a7215b787a62bd5fba0e22d
Buse Yılmaz <bsylm@outlook.com>
2023-09-03 15:47:17 +0300

    Yapilan

--- Assets/Script/PassScene.cs
+++ Assets/Script/PassScene.cs
@@ -5,9 +5,18 @@ using UnityEngine.SceneManagement;
 
 public class PassScene : MonoBehaviour
 {
+    public void Basla(int sahenID)
+    {
+        SceneManager.LoadScene(0); // Belirlediðiniz sahneyi yükler
+    }
     public void Yenile(int sahenID)
     {
         SceneManager.LoadScene(0); // Belirlediðiniz sahneyi yükler
+    }
+
+    public void Krediler(int sahenID)
+    {
+        SceneManager.LoadScene(2); // Belirlediðiniz sahneyi yükler
     }
 
     public void Cikis()
