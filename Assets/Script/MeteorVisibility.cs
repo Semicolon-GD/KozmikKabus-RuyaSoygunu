0876059117a1957007d18765a1c86b383d35359d
Buse Yılmaz <bsylm@outlook.com>
2023-09-03 14:54:19 +0300

    SahneGecisEklendi
    SahneGecisEklendi

--- Assets/Script/MeteorVisibility.cs
+++ Assets/Script/MeteorVisibility.cs
@@ -1,8 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MeteorVisibility : MonoBehaviour
 {
-   
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player")) // Uzay gemisi veya baþka bir etiket kullanabilirsiniz
+        {
+            // Debug.Log("Yeni sahne");
+            // Büyük meteorun Collider'ý ile temas saðlandýðýnda yeni sahneye geçin
+            //SceneManager.LoadScene(""); // Yeni sahnenin adýný belirtin
+        }
+    }
 }
